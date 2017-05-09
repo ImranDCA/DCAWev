d6ddfa35300bbadd57645bc3aec06b24e7735fd2
ImranDCA <imran.chowdhury@doctorcareanywhere.com>
2017-05-09 10:32:46 +0100

    changed the project file

---
+++ bDtoProvider/Account.cs
@@ -0,0 +1,18 @@
+﻿using RepositoryProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoProvider
+{
+    public class AccountDto : IDCEntity
+    {
+        public Guid EntityId { get; set; }
+
+        public string EntityLogicalName { get; set; }
+
+        public string EntityPluralName { get; set; }
+    }
+}
